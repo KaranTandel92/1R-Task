7bd94b126d3a55b79e807ab68c781f7153438801
Karan <karan.tandel92@gmail.com>
2023-03-02 19:24:06 +0530

    feat : make code on regex

--- task/javascript/form_with_table/assets/js/table.js
+++ task/javascript/form_with_table/assets/js/table.js
@@ -2,6 +2,7 @@ let data =
     { id: "ID", name: "NAME", technology: "TECHNOLOGY", action: "ACTION" };
 
 let formfirst = document.getElementById("form");
+
 formfirst.addEventListener("submit", (event) => {
     event.preventDefault();
     let fullname = document.getElementById("firstname");
@@ -23,8 +24,14 @@ formfirst.addEventListener("submit", (event) => {
         .then(data => console.log(data))
         .catch(error => console.log(error));
     // return dataStr;
+    tableBody();
 });
 
+
+window.addEventListener("load", (e) => {
+    e.preventDefault();
+    tableBody();
+})
 //create a table
 let table = document.createElement("table");
 table.className = "table";
@@ -48,62 +55,68 @@ createTableHeading(table, data);
 // end table heading
 
 // make table body
-fetch("http://localhost:3000/newEmployee").then(response => response.json()).then(data => {
-    let tbody = document.createElement("tbody");
-    table.appendChild(tbody);
-    for (const element of data) {
-        let tr = document.createElement("tr");
-        tr.className = "tr";
-        tbody.appendChild(tr);
+let tbody = document.createElement("tbody");
+table.appendChild(tbody);
 
-        let td1 = document.createElement("td");
-        td1.className = "td";
-        let td1Text = document.createTextNode(element["id"]);
-        td1.appendChild(td1Text);
-        tr.appendChild(td1);
 
-        let td2 = document.createElement("td");
-        td2.className = "td2";
-        let td2Text = document.createTextNode(element["fullname"]);
-        td2.appendChild(td2Text);
-        tr.appendChild(td2);
+function tableBody() {
+    tbody.innerHTML = " "
+    fetch("http://localhost:3000/newEmployee").then(response => response.json()).then(data => {
+        for (const element of data) {
 
-        let td3 = document.createElement("td");
-        td3.className = "td3";
-        let td3Text = document.createTextNode(element["technology"]);
-        td3.appendChild(td3Text);
-        tr.appendChild(td3);
+            let tr = document.createElement("tr");
+            tr.className = "tr";
+            tbody.appendChild(tr);
 
-        // create delete button
-        let td4 = document.createElement("td");
-        td4.className = "td4";
-        let deleteButton = document.createElement("button");
-        deleteButton.className = "delete-button";
-        let deleteText = document.createTextNode("Delete");
-        deleteButton.appendChild(deleteText);
-        deleteButton.addEventListener("click", e => {
-            fetch(`http://localhost:3000/newEmployee/${element["id"]}`, { method: "DELETE" }) // data delete from the json server (or data base)
-            // .catch(err => console.log(err));
-            e.preventDefault();
-            table.deleteRow(tr.rowIndex); // row delete from the table
-        })
-        td4.appendChild(deleteButton);
-        tr.appendChild(td4);
+            let td1 = document.createElement("td");
+            td1.className = "td";
+            let td1Text = document.createTextNode(element["id"]);
+            td1.appendChild(td1Text);
+            tr.appendChild(td1);
 
-        // create edit button
-        let td5 = document.createElement("td");
-        td5.className = "td";
-        let editButton = document.createElement("button");
-        editButton.className = "edit-button";
-        let editText = document.createTextNode("Edit");
-        editButton.appendChild(editText);
-        editButton.addEventListener("click", (e) => {
-            rowEdit(element)
-        });
-        td5.appendChild(editButton);
-        tr.appendChild(td5);
-    }
-}).catch(err => console.log(err))
+            let td2 = document.createElement("td");
+            td2.className = "td2";
+            let td2Text = document.createTextNode(element["fullname"]);
+            td2.appendChild(td2Text);
+            tr.appendChild(td2);
+
+            let td3 = document.createElement("td");
+            td3.className = "td3";
+            let td3Text = document.createTextNode(element["technology"]);
+            td3.appendChild(td3Text);
+            tr.appendChild(td3);
+
+            // create delete button
+            let td4 = document.createElement("td");
+            td4.className = "td4";
+            let deleteButton = document.createElement("button");
+            deleteButton.className = "delete-button";
+            let deleteText = document.createTextNode("Delete");
+            deleteButton.appendChild(deleteText);
+            deleteButton.addEventListener("click", e => {
+                fetch(`http://localhost:3000/newEmployee/${element["id"]}`, { method: "DELETE" }) // data delete from the json server (or data base)
+                // .catch(err => console.log(err));
+                e.preventDefault();
+                table.deleteRow(tr.rowIndex); // row delete from the table
+            })
+            td4.appendChild(deleteButton);
+            tr.appendChild(td4);
+
+            // create edit button
+            let td5 = document.createElement("td");
+            td5.className = "td";
+            let editButton = document.createElement("button");
+            editButton.className = "edit-button";
+            let editText = document.createTextNode("Edit");
+            editButton.appendChild(editText);
+            editButton.addEventListener("click", (e) => {
+                rowEdit(element)
+            });
+            td5.appendChild(editButton);
+            tr.appendChild(td5);
+        }
+    }).catch(err => console.log(err))
+}
 //end table body
 
 let updateButton = document.getElementById("updatebutton");
@@ -132,6 +145,68 @@ function rowEdit(element) {
             .then(data => console.log(data))
     })
 };
+
+let searchMenu = document.getElementById("searchMenu");
+searchMenu.addEventListener("keyup", (event) => {
+    event.preventDefault();
+    fetch("http://localhost:3000/newEmployee").then(response => response.json()).then(data => {
+        let array = data.filter(item => searchMenu.value == " " || item.fullname.toLowerCase().includes(searchMenu.value) || item.technology.includes(searchMenu.value));
+        tbody.innerHTML = " "
+        for (const element of array) {
+            let tr = document.createElement("tr");
+            tr.className = "tr";
+            tbody.appendChild(tr);
+
+            let td1 = document.createElement("td");
+            td1.className = "td";
+            let td1Text = document.createTextNode(element["id"]);
+            td1.appendChild(td1Text);
+            tr.appendChild(td1);
+
+            let td2 = document.createElement("td");
+            td2.className = "td2";
+            let td2Text = document.createTextNode(element["fullname"]);
+            td2.appendChild(td2Text);
+            tr.appendChild(td2);
+
+            let td3 = document.createElement("td");
+            td3.className = "td3";
+            let td3Text = document.createTextNode(element["technology"]);
+            td3.appendChild(td3Text);
+            tr.appendChild(td3);
+
+            // create delete button
+            let td4 = document.createElement("td");
+            td4.className = "td4";
+            let deleteButton = document.createElement("button");
+            deleteButton.className = "delete-button";
+            let deleteText = document.createTextNode("Delete");
+            deleteButton.appendChild(deleteText);
+            deleteButton.addEventListener("click", e => {
+                fetch(`http://localhost:3000/newEmployee/${element["id"]}`, { method: "DELETE" }) // data delete from the json server (or data base)
+                // .catch(err => console.log(err));
+                e.preventDefault();
+                table.deleteRow(tr.rowIndex); // row delete from the table
+            })
+            td4.appendChild(deleteButton);
+            tr.appendChild(td4);
+
+            // create edit button
+            let td5 = document.createElement("td");
+            td5.className = "td";
+            let editButton = document.createElement("button");
+            editButton.className = "edit-button";
+            let editText = document.createTextNode("Edit");
+            editButton.appendChild(editText);
+            editButton.addEventListener("click", (e) => {
+                rowEdit(element)
+            });
+            td5.appendChild(editButton);
+            tr.appendChild(td5);
+        }
+        console.log(array);
+    });
+})
 
 let body = document.querySelector("body");
 body.appendChild(table);
--- task/javascript/regex/assets/js/index.js
+++ task/javascript/regex/assets/js/index.js
@@ -1,141 +1,215 @@
+function mainData() {
 
-// let submit = document.getElementById("submit");
+    //first name
+    let firstName = document.getElementById("firstName");
 
-//first name
-let firstName = document.getElementById("firstName");
+    firstName.addEventListener("keyup", () => {
+        if (firstName.value.trim().match(/[0-9]/)) {
+            firstName.style.border = "2px solid red"
+            document.getElementById("userError").innerHTML = "Enter valid name"
+            document.getElementById("userError").classList.add("color-red");
+        }
+        else if (firstName.value == "") {
+            firstName.style.border = "2px solid red"
+            document.getElementById("userError").innerHTML = "Name is require"
+            document.getElementById("userError").classList.add("color-red");
+        }
+        else {
+            firstName.style.border = "2px solid green";
+            document.getElementById("userError").innerHTML = ""
+        }
+    })
+    // end first name
 
-firstName.addEventListener("keyup", () => {
-    if (firstName.value.trim().match(/[0-9]/)) {
-        firstName.style.border = "2px solid red"
-        document.getElementById("user-error").innerHTML = "Enter valid name"
-        document.getElementById("user-error").classList.add("color-red");
-    }
-    else if (firstName.value == "") {
-        firstName.style.border = "2px solid red"
-        document.getElementById("user-error").innerHTML = "Name is require"
-        document.getElementById("user-error").classList.add("color-red");
-    }
-    else {
-        firstName.style.border = "2px solid green";
-    }
-})
-// end first name
+    //last name
+    let lastName = document.getElementById("lastName");
 
-//last name
-let lastName = document.getElementById("lastName");
+    lastName.addEventListener("keyup", () => {
+        if (lastName.value.trim().match(/[0-9]/)) {
+            lastName.style.border = "2px solid red"
+            document.getElementById("lastNameError").innerHTML = "Enter valid name"
+            document.getElementById("lastNameError").classList.add("color-red");
+        }
+        else if (lastName.value == "") {
+            lastName.style.border = "2px solid red"
+            document.getElementById("lastNameError").innerHTML = "Name is require"
+            document.getElementById("lastNameError").classList.add("color-red");
+        }
+        else {
+            lastName.style.border = "2px solid green";
+            document.getElementById("lastNameError").innerHTML = ""
+        }
+    })
+    //     //end last name
 
-lastName.addEventListener("keyup", () => {
-    if (/[A-Z]{1}[a-z]{2,12}/.test(lastName.value.trim())) {
-        console.log(lastName.value.trim());
-    }
-    else if (lastName.value.trim().match(/[0-9]/)) {
-        lastName.style.border = "2px solid red";
-        document.getElementById("last-name-error").innerHTML = "* Enter valid last name";
-        document.getElementById("last-name-error").classList.add("color-red");
-    }
-    else if (lastName.value == "") {
-        lastName.style.border = "2px solid red"
-        document.getElementById("last-name-error").innerHTML = "Last name is require"
-        document.getElementById("last-name-error").classList.add("color-red");
-    }
-    else {
-        lastName.style.border = "2px solid green";
-    }
-})
-//     //end last name
+    //     //phone number
+    let phone = document.getElementById("phoneNumber");
 
-//     //phone number
-let phone = document.getElementById("phoneNumber");
+    phone.addEventListener("keyup", () => {
+        if (/^[89][0-9]{9}$/.test(phone.value)) {
+            phone.style.border = "2px solid green";
+            // console.log(phone.value);
+            document.getElementById("mobileNumber").innerHTML = "";
+        }
+        else if (phone.value.trim().match(/[^8-9]/)) {
+            phone.style.border = "2px solid red";
+            document.getElementById("mobileNumber").innerHTML = "* Enter valid number";
+            document.getElementById("mobileNumber").classList.add("color-red");
+        }
+        else if (phone.value.match(/^[^0-9]/)) {
+            phone.style.border = "2px solid red"
+            document.getElementById("mobileNumber").innerHTML = "Number only"
+            document.getElementById("mobileNumber").classList.add("color-red");
+        }
+        else if (phone.value == "") {
+            phone.style.border = "2px solid red"
+            document.getElementById("mobileNumber").innerHTML = "Mobile number should be compulsory"
+            document.getElementById("mobileNumber").classList.add("color-red");
+        }
+        else {
+            return true;
+        }
+    })
+    //end phone number
 
-phone.addEventListener("keyup", () => {
-    if (/[89][0-9]{9}/.test(phone.value)) {
-        console.log(phone.value);
-    }
-    else if (phone.value.trim().match(/[^8-9]/)) {
-        phone.style.border = "2px solid red";
-        document.getElementById("mobile-number").innerHTML = "* Enter valid number";
-        document.getElementById("mobile-number").classList.add("color-red");
-    }
-    else if (phone.value.match(/^[^0-9]/)) {
-        phone.style.border = "2px solid red"
-        document.getElementById("mobile-number").innerHTML = "Number only"
-        document.getElementById("mobile-number").classList.add("color-red");
-    }
-    else if (phone.value == "") {
-        phone.style.border = "2px solid red"
-        document.getElementById("mobile-number").innerHTML = "Mobile number should be compulsory"
-        document.getElementById("mobile-number").classList.add("color-red");
-    }
-    else {
-        phone.style.border = "2px solid green";
-    }
-})
-//end phone number
+    //eamil address
+    //for personal
+    let email = document.getElementById("email");
 
-//eamil address
-//for personal
-let email = document.getElementById("email");
+    email.addEventListener("keyup", () => {
+        if (/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}$/.test(email.value)) {
+            email.style.border = "2px solid green";
+            document.getElementById("emailAddress").innerHTML = ""
+            // console.log(email.value);
+        }
+        else if (email.value == "") {
+            email.style.border = "2px solid red"
+            document.getElementById("emailAddress").innerHTML = "Email id is require"
+            document.getElementById("emailAddress").classList.add("color-red");
+        }
+        else {
+            return true;
+        }
+    })
 
-email.addEventListener("keyup", () => {
-    if (/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}$/.test(email.value)) {
-        console.log(email.value);
-    }
-    else if (email.value == "") {
-        email.style.border = "2px solid red"
-        document.getElementById("email-address").innerHTML = "Email id is require"
-        document.getElementById("email-address").classList.add("color-red");
-    }
-    else {
-        email.style.border = "2px solid green";
-    }
-})
+    //for organization
+    let anotherEmail = document.getElementById("anotherEmail");
 
-//for organization
-let anotherEmail = document.getElementById("anotherEmail");
-
-anotherEmail.addEventListener("keyup", () => {
-    if (/^[A-Za-z0-9._%+-]+@1Rivet.com$/.test(anotherEmail.value)) {
-        console.log(anotherEmail.value);
-    }
-    else if (anotherEmail.value == "") {
-        anotherEmail.style.border = "2px solid red"
-        document.getElementById("another-email-address").innerHTML = "Email id is require"
-        document.getElementById("another-email-address").classList.add("color-red");
-    }
-    else {
-        anotherEmail.style.border = "2px solid green";
-    }
-})
-
-//     //end email address
+    anotherEmail.addEventListener("keyup", () => {
+        if (/^[A-Za-z0-9._%+-]+@1Rivet.com$/.test(anotherEmail.value)) {
+            anotherEmail.style.border = "2px solid green";
+            // console.log(anotherEmail.value);
+            document.getElementById("anotherEmailAddress").innerHTML = ""
+        }
+        else if (anotherEmail.value == "") {
+            anotherEmail.style.border = "2px solid red"
+            document.getElementById("anotherEmailAddress").innerHTML = "Email id is require"
+            document.getElementById("anotherEmailAddress").classList.add("color-red");
+        }
+        else if (/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}$/.test(anotherEmail.value)) {
+            anotherEmail.style.border = "2px solid red"
+            document.getElementById("anotherEmailAddress").innerHTML = "Enter valid mail id"
+            document.getElementById("anotherEmailAddress").classList.add("color-red");
+        }
+        else {
+            return true;
+        }
+    })
+    //     //end email address
 
 
-//     //passwaord
-// let password = document.getElementById("password");
+    //     //passwaord
+    let password = document.getElementById("password");
 
-// let reg = /[a-zA-Z0-9!@#$%^&*]{6,16}/;
-// if (reg.test(password.value)) {
-//     console.log(password.value);
-// }
-// else {
-//     password.style.border = "2px solid red"
+    password.addEventListener("keyup", () => {
+        // let reg = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+        let reg = /[0-9]/;
+        if (reg.test(password.value)) {
+            // console.log(password.value);
+            // document.getElementById("userPassword").innerHTML = "Enter valid password"
+            // document.getElementById("userPassword").classList.add("color-red");
+            password.style.border = "2px solid green";
+            document.getElementById("userPassword").innerHTML = "";
+        }
+        else if (password.value == "") {
+            password.style.border = "2px solid red"
+            document.getElementById("userPassword").innerHTML = "Please enter your password"
+            document.getElementById("userPassword").classList.add("color-red");
+        }
+        else {
+            password.style.border = "2px solid red";
+        }
+    })
+    //end password
 
-// }
-//     //end password
+    //     //salary
+    let salary = document.getElementById("salary");
 
-//     //salary
-let salary = document.getElementById("salary");
+    salary.addEventListener("keyup", () => {
+        if (salary.value >= 5000) {
+            console.log(salary.value);
+            salary.style.border = "2px solid green";
+            document.getElementById("userSalary").innerHTML = "";
+        }
+        else if (salary.value < 5000) {
+            salary.style.border = "2px solid red"
+            document.getElementById("userSalary").innerHTML = "Enter valid amount"
+            document.getElementById("userSalary").classList.add("color-red");
+        }
+        else {
+            salary.style.border = "2px solid green";
+        }
+    })
+    //     //end : salary
 
-salary.addEventListener("keyup", () => {
-    if (salary.value >= 5000) {
-        console.log(salary.value);
-        salary.style.border = "2px solid green";
-        document.getElementById("user-salary").innerHTML = "Email id is require"
-        document.getElementById("user-salary").classList.add("color-red");
-    }
-    else {
-        salary.style.border = "2px solid red"
-    }
-})
-//     //end : salary
+    submit.addEventListener("click", (e) => {
+        e.preventDefault();
+        let firstName = document.getElementById("firstName").value;
+        let lastName = document.getElementById("lastName").value;
+        let phone = document.getElementById("phoneNumber").value;
+        let email = document.getElementById("email").value;
+        let anotherEmail = document.getElementById("anotherEmail").value;
+        let password = document.getElementById("password").value;
+        let salary = document.getElementById("salary").value;
+        if (firstName == "" || lastName == "" || phone == "" || email == "" || anotherEmail == "" || password == "" || salary == "") {
+            alert("error")
+        }
+
+        else if (userError.innerHTML == "" && lastNameError.innerHTML == "" &&
+            mobileNumber.innerHTML == "" && emailAddress.innerHTML == "" &&
+            anotherEmailAddress.innerHTML == "" && userPassword.innerHTML == "" &&
+            userSalary.innerHTML == "") {
+
+            let firstName = document.getElementById("firstName");
+            let lastName = document.getElementById("lastName");
+            let phone = document.getElementById("phoneNumber");
+            let email = document.getElementById("email");
+            let anotherEmail = document.getElementById("anotherEmail");
+            let password = document.getElementById("password");
+            let salary = document.getElementById("salary");
+            fetch(" http://localhost:3000/newRegex", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    firstName: firstName.value,
+                    lastName: lastName.value,
+                    phone: phone.value,
+                    email: email.value,
+                    anotherEmail: anotherEmail.value,
+                    password: password.value,
+                    salary: salary.value
+                })
+            }).then((response) => response.json()).then(data => console.log(data));
+            // Go to next page
+            let submit = document.getElementById("submit");
+
+            // Store data
+            window.location.href = "link.html"
+        }
+    })
+
+}
+mainData();
 
