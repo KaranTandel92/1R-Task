f764e1863511a264575ea31dfa4f638dd75639a1
Karan <karan.tandel92@gmail.com>
2023-03-06 19:22:43 +0530

    fix:modify of assessment_2

--- task/assessment_ 2/assets/js/index.js
+++ task/assessment_ 2/assets/js/index.js
@@ -1,32 +1,34 @@
 let saveButton = document.querySelector(".saveButton");
+let form = document.querySelector(".mainForm");
 
-saveButton.addEventListener("click", (event) => {
-    let name = document.getElementById("userName");
-    let description = document.getElementById("writeDescription");
-    let selectOption = document.getElementById("dataStatus");
-    let rate = document.getElementById("writeRate");
-    let balance = document.getElementById("checkBalance");
-    let deposit = document.getElementById("userDeposite");
+// saveButton.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     let name = document.getElementById("userName");
+//     let description = document.getElementById("writeDescription");
+//     let selectOption = document.getElementById("dataStatus");
+//     let rate = document.getElementById("writeRate");
+//     let balance = document.getElementById("checkBalance");
+//     let deposit = document.getElementById("userDeposite");
 
-    let postData = {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            name: name.value,
-            description: description.value,
-            selectOption: selectOption.value,
-            rate: rate.value,
-            balance: balance.value,
-            deposit: deposit.value
-        })
-    }
-    fetch("http://localhost:3000/dataStore", postData)
-        .then((response) => (response.json()))
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
-})
+//     let postData = {
+//         method: "POST",
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//             name: name.value,
+//             description: description.value,
+//             selectOption: selectOption.value,
+//             rate: rate.value,
+//             balance: balance.value,
+//             deposit: deposit.value
+//         })
+//     }
+//     fetch("http://localhost:3000/dataStore", postData)
+//         .then((response) => (response.json()))
+//         .then(data => console.log(data))
+//         .catch(error => console.log(error));
+// })
 
 let header = { id: "#", name: "NAME", description: "DESCRIPTION", selectOption: "STATUS", rate: "RATE", balance: "BALANCE", deposit: "DEPOSITE" }
 
@@ -36,7 +38,6 @@ function CreateTableHeading(table, data) {
     let thead = document.createElement("thead");
     table.appendChild(thead);
     let tr = document.createElement("tr");
-    tr.className = "tableHeadingRow"
     thead.appendChild(tr);
     for (const key in data) {
         let th = document.createElement("th");
@@ -57,50 +58,85 @@ CreateTableHeading(table, header);
 async function checkUserHosting() {
     let req = await fetch('http://localhost:3000/dataStore');
     let res = await req.json();
-    return res;
+    createTableBody(res);
 };
-checkUserHosting().then(data => console.log(data));
+
+window.addEventListener("load", (e) => {
+    checkUserHosting()
+})
 
 // // create table body
-function createTableBody(table) {
+function createTableBody(data) {
+
     let tbody = document.createElement("tbody");
     table.appendChild(tbody);
-    checkUserHosting().then(data => {
-        for (const element of data) {
-            let tr = document.createElement("tr");
-            tbody.appendChild(tr);
-            for (const item in header) {
-                let td = document.createElement("td");
-                td.className = "tableData";
-                let text = document.createTextNode(element[item]);
-                td.appendChild(text);
-                tr.appendChild(td);
-            }
-            let td = document.createElement("td");
-            tr.appendChild(td);
 
-            let deleteButton = document.createElement("button");
-            deleteButton.className = "delete-button";
-            let deleteText = document.createTextNode("Delete");
-            deleteButton.addEventListener("click", e => {
-                fetch(`http://localhost:3000/dataStore/${element["id"]}`, { method: "DELETE" })
-                table.deleteRow(tr.rowIndex);
-            })
-            deleteButton.appendChild(deleteText);
-            td.appendChild(deleteButton);
-            let editButton = document.createElement("button");
-            editButton.className = "edit-button";
-            let editText = document.createTextNode("Edit");
-            editButton.addEventListener("click", e => {
-                rowEdit(element)
-            })
-            editButton.appendChild(editText);
-            td.appendChild(editButton);
+    for (const element of data) {
+        let tr = document.createElement("tr");
+        tbody.appendChild(tr);
+        for (const item in header) {
+            let td = document.createElement("td");
+            td.className = "tableData";
+            let text = document.createTextNode(element[item]);
+            if (item == "name") {
+                td.className = "nameOfUser";
+            }
+            if (item == "rate") {
+                td.className = "rateOfUser";
+            }
+            if (item == "deposit") {
+                td.className = "depositOfUser";
+            }
+            if (item == "description") {
+                td.className = "userDescription";
+            }
+            if (item == "selectOption") {
+                let span = document.createElement("span");
+                // console.log(item);
+                if (element[item] == "Error") {
+                    span.className = "errorOption";
+                }
+                else if (element[item] == "Open") {
+                    span.className = "openOption"
+                }
+                else if (element[item] == "Success") {
+                    span.className = "successOption"
+                }
+                else if (element[item] == "Inactive") {
+                    span.className = "inactiveOption"
+                }
+                span.appendChild(text);
+                td.appendChild(span);
+            }
+            else {
+                td.appendChild(text);
+            }
+            if (item == "balance") {
+                td.className = "userCurrentBalance";
+            }
+            tr.appendChild(td);
         }
-    });
+        let td = document.createElement("td");
+        tr.appendChild(td);
+        let deleteButton = document.createElement("button");
+        deleteButton.className = "delete-button";
+        let deleteText = document.createTextNode("Delete");
+        deleteButton.addEventListener("click", e => {
+            fetch(`http://localhost:3000/dataStore/${element["id"]}`, { method: "DELETE" })
+            table.deleteRow(tr.rowIndex);
+        })
+        deleteButton.appendChild(deleteText);
+        td.appendChild(deleteButton);
+        let editButton = document.createElement("button");
+        editButton.className = "edit-button";
+        let editText = document.createTextNode("Edit");
+        editButton.addEventListener("click", e => {
+            rowEdit(element)
+        })
+        editButton.appendChild(editText);
+        td.appendChild(editButton);
+    }
 }
-createTableBody(table);
-// })
 // end : table body
 
 let updateButton = document.querySelector(".updateButton");
@@ -141,8 +177,14 @@ function rowEdit(element) {
     })
 }
 // start : validation
-userName.addEventListener("keyup", () => {
-    if (userName.value.trim().match(/[^a-zA-Z]/)) {
+
+function userNameValidation() {
+    if (userName.value.length < 3 || userName.value.length >= 25) {
+        userName.style.border = "2px solid red"
+        document.getElementById("userError").innerHTML = "Length should be 3 to 25"
+        document.getElementById("userError").classList.add("color-red");
+    }
+    else if (!userName.value.match(/^[a-zA-Z]*$/)) {
         userName.style.border = "2px solid red"
         document.getElementById("userError").innerHTML = "Enter valid name"
         document.getElementById("userError").classList.add("color-red");
@@ -152,31 +194,38 @@ userName.addEventListener("keyup", () => {
         document.getElementById("userError").innerHTML = "Name is require"
         document.getElementById("userError").classList.add("color-red");
     }
-    else if (userName.value.trim().match(/[a-zA-Z]{3,25}/)) {
+    else {
         userName.style.border = "2px solid green";
         document.getElementById("userError").innerHTML = ""
-    }
-    else {
         return true;
     }
+}
+userName.addEventListener("keyup", (e) => {
+    e.preventDefault()
+    userNameValidation();
 })
 
-writeDescription.addEventListener("keyup", () => {
+function descriptionValidation() {
     if (writeDescription.value == "") {
         writeDescription.style.border = "2px solid red"
         document.getElementById("descriptionError").innerHTML = "Length should be min 3 and max 150 characters"
         document.getElementById("descriptionError").classList.add("color-red");
     }
-    else if (writeDescription.value.trim().match(/[a-zA-Z0-9]{3,150}/)) {
-        writeDescription.style.border = "2px solid green";
-        document.getElementById("descriptionError").innerHTML = ""
+    else if (!writeDescription.value.match(/[^a-zA-Z\s]*/)) {
+        writeDescription.style.border = "2px solid red";
+        document.getElementById("descriptionError").innerHTML = "enter valid description"
     }
     else {
+        writeDescription.style.border = "2px solid green";
+        document.getElementById("userError").innerHTML = ""
         return true;
     }
+}
+writeDescription.addEventListener("keyup", () => {
+    descriptionValidation();
 })
 
-dataStatus.addEventListener("change", () => {
+function optionValidation() {
     if (dataStatus.value == "") {
         dataStatus.style.border = "2px solid red"
         document.getElementById("optionError").innerHTML = "Select your status"
@@ -184,76 +233,88 @@ dataStatus.addEventListener("change", () => {
     }
     else {
         dataStatus.style.border = "2px solid green";
-        document.getElementById("userError").innerHTML = ""
+        document.getElementById("optionError").innerHTML = ""
         return true;
     }
+}
+dataStatus.addEventListener("change", () => {
+    optionValidation();
 })
 
-writeRate.addEventListener("keyup", () => {
+function rateValidation() {
     if (writeRate.value == "") {
         writeRate.style.border = "2px solid red"
         document.getElementById("rateError").innerHTML = "This Field is Required"
         document.getElementById("rateError").classList.add("color-red");
     }
-    else if (writeRate.value.trim().match(/[^$0-9]/)) {
+    else if (!writeRate.value.trim().match(/^[0-9]*$/)) {
         document.getElementById("rateError").innerHTML = "Enter valid amount"
         document.getElementById("rateError").classList.add("color-red");
     }
-    else if (writeRate.value.trim().match(/[$0-9]/)) {
+    else {
         writeRate.style.border = "2px solid green";
         document.getElementById("rateError").innerHTML = ""
-    }
-    else {
+
         return true;
     }
+}
+writeRate.addEventListener("keyup", () => {
+    rateValidation();
 })
 
-checkBalance.addEventListener("keyup", () => {
+function balanceValidation() {
+
     if (checkBalance.value == "") {
         checkBalance.style.border = "2px solid red"
         document.getElementById("balanceError").innerHTML = "This Field is Required"
         document.getElementById("balanceError").classList.add("color-red");
     }
-    else if (checkBalance.value.trim().match(/[^$0-9]/)) {
+    else if (!checkBalance.value.trim().match(/^[0-9]*$/)) {
         document.getElementById("balanceError").innerHTML = "Enter valid amount"
         document.getElementById("balanceError").classList.add("color-red");
     }
-    else if (checkBalance.value.trim().match(/[$0-9]/)) {
-        checkBalance.style.border = "2px solid green";
-        document.getElementById("balanceError").innerHTML = ""
-    }
     else {
+        checkBalance.style.border = "2px solid green";
+        document.getElementById("balanceError").innerHTML = "";
         return true;
     }
+}
+checkBalance.addEventListener("keyup", () => {
+    balanceValidation();
 })
 
-userDeposite.addEventListener("keyup", () => {
+function depositeValidation() {
     if (userDeposite.value == "") {
         userDeposite.style.border = "2px solid red"
         document.getElementById("depositError").innerHTML = "This Field is Required"
         document.getElementById("depositError").classList.add("color-red");
     }
-    else if (userDeposite.value.trim().match(/[^$0-9]/)) {
+    else if (!userDeposite.value.trim().match(/^[0-9]*$/)) {
         document.getElementById("depositError").innerHTML = "Enter valid amount"
         document.getElementById("depositError").classList.add("color-red");
     }
-    else if (userDeposite.value.trim().match(/[$0-9]/)) {
-        userDeposite.style.border = "2px solid green";
-        document.getElementById("depositError").innerHTML = ""
-    }
     else {
+        userDeposite.style.border = "2px solid green";
+        document.getElementById("depositError").innerHTML = "";
         return true;
     }
+}
+
+userDeposite.addEventListener("keyup", () => {
+    depositeValidation();
 })
 saveButton.addEventListener("click", (e) => {
-    let name = document.getElementById("userName").value;
-    let description = document.getElementById("writeDescription").value;
-    let selectOption = document.getElementById("dataStatus").value;
-    let rate = document.getElementById("writeRate").value;
-    let balance = document.getElementById("checkBalance").value;
-    let deposit = document.getElementById("userDeposite").value;
-    if (name == "" || description == "" || selectOption == "" || rate == "" || balance == "" || deposit == "") {
-        alert("Submit valid data")
+    // debugger
+    // e.preventDefault();
+    let name = document.getElementById("userName");
+    let description = document.getElementById("writeDescription");
+    let selectOption = document.getElementById("dataStatus");
+    let rate = document.getElementById("writeRate");
+    let balance = document.getElementById("checkBalance");
+    let deposit = document.getElementById("userDeposite");
+
+    if (depositeValidation() && balanceValidation() && rateValidation() && optionValidation() && descriptionValidation() && userNameValidation()) {
+
         fetch("http://localhost:3000/dataStore", {
             method: "POST",
             headers: {
@@ -268,8 +329,10 @@ saveButton.addEventListener("click", (e) => {
                 deposit: deposit.value
             })
         }).then((response) => response.json()).then(data => console.log(data));
+    } else {
+        alert("Submit valid data")
     }
-})
+});
 
 
 
